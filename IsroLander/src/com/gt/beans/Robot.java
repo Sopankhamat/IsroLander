70f63147cc18461377369205e79064a7ed16cefc
SopanKhamat <gajanan.tech86@gmail.com>
2019-09-07 11:43:26 -0700

    added class Robot.java

---
+++ IsroLander/src/com/gt/beans/Robot.java
@@ -0,0 +1,25 @@
+package com.gt.beans;
+
+public class Robot {
+	private String id;
+	private String name;
+	private String chip;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getChip() {
+		return chip;
+	}
+	public void setChip(String chip) {
+		this.chip = chip;
+	}
+}
